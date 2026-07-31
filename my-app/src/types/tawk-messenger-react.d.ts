ecda7b4ae4add67778d1f8d5ec0ba6d20466edbc
naveenalla3000 <naveenalla3000@gmail.com>
2026-07-31 22:10:52 +0530

    fix: remove obsolete Tawk React dependency

--- my-app/src/types/tawk-messenger-react.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@tawk.to/tawk-messenger-react' {
-  import { ComponentType } from 'react';
-  const TawkMessengerReact: ComponentType<{ propertyId: string; widgetId: string }>;
-  export default TawkMessengerReact;
-}
